f4231a2c8845cfd64601aa7b64c61de8afdf1b2b
Christopher Shea <cmshea@gmail.com>
2019-02-05 16:45:44 -0500

    Add view templating

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var us_states = require('../us_state.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Find My Election', states: us_states });
 });
 
 module.exports = router;
