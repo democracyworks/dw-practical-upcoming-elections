63542677489c45d29ec0579e0e8e368fd8cd220d
Katrina Hall <katrina.m.hall@gmail.com>
2019-02-12 16:34:56 -0500

    adding initial python template work

---
+++ python/README.md
@@ -0,0 +1,10 @@
+# Upcoming Elections Practical
+Instructions for installing Flask can be found [here](http://flask.pocoo.org/docs/1.0/installation/)
+
+## Running
+
+export FLASK_APP=elections
+export FLASK_ENV=development
+flask run
+
+## Testing
---
+++ python/elections/upcoming.py
@@ -0,0 +1,26 @@
+import functools
+
+from us_states import states
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+# from werkzeug.security import check_password_hash, generate_password_hash
+
+# from flaskr.db import get_db
+
+bp = Blueprint('address_form', __name__, url_prefix='/')
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def search():
+    """Take in an address."""
+    if request.method == 'POST':
+        address1 = request.form['street']
+        address2 = request.form['street-2']
+        city = request.form['city']
+        state = request.form['state']
+        zip = request.form['zip']
+        return render_template('election_results.html')
+
+    return render_template('address_form.html', states=states)
