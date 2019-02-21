f8cbb565480ebec49e548d6208708479af179edc
Katrina Hall <katrina.m.hall@gmail.com>
2019-02-21 09:55:29 -0500

    removing commented out and otherwise unnecessary code

--- python/elections/__init__.py
+++ python/elections/__init__.py
@@ -23,11 +23,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
 
     from elections import upcoming
     app.register_blueprint(upcoming.bp)
--- python/elections/upcoming.py
+++ python/elections/upcoming.py
@@ -5,9 +5,6 @@ from elections.us_states import postal_abbreviations
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-# from werkzeug.security import check_password_hash, generate_password_hash
-
-# from flaskr.db import get_db
 
 bp = Blueprint('address_form', __name__, url_prefix='/')
 
@@ -16,11 +13,6 @@ bp = Blueprint('address_form', __name__, url_prefix='/')
 def search():
     """Take in an address."""
     if request.method == 'POST':
-        address1 = request.form['street']
-        address2 = request.form['street-2']
-        city = request.form['city']
-        state = request.form['state']
-        zip = request.form['zip']
         return render_template('election_results.html')
 
     return render_template('address_form.html', states=states)
