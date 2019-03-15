9b5e47658d532497c0a2a78cd94e12b972033dff
Kyle <kyle.rozanitis@gmail.com>
2019-03-15 15:07:26 -0400

    Add template to display elections

--- python/elections/upcoming.py
+++ python/elections/upcoming.py
@@ -11,8 +11,15 @@ bp = Blueprint('address_form', __name__, url_prefix='/')
 
 @bp.route('/', methods=('GET', 'POST'))
 def search():
-    """Take in an address."""
     if request.method == 'POST':
-        return render_template('election_results.html')
+        redirect(url_for('search'))
 
     return render_template('address_form.html', states=postal_abbreviations)
+
+
+@bp.route('/search', methods=('POST',))
+def do_something():
+    if request.method == 'POST':
+        city_name = str(request.form['city'])
+        state_name = str(request.form['state'])
+        return render_template('upcoming_elections.html', city=city_name, state=state_name)
