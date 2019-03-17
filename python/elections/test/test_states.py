11525ca8e76c58c0e697e12f89177a082138afb3
Kyle <kyle.rozanitis@gmail.com>
2019-03-17 13:00:03 -0400

    Add unittest package and modify postal_abbrev test

--- python/elections/test/test_states.py
+++ python/elections/test/test_states.py
@@ -1,10 +1,24 @@
+""" This file contains the unit tests for querying data, collecting data
+in a local repository, and ensuring that the length of the postal codes list == 61 and the states with current DemocracyWorks employees are in
+the list """
+
+# Library Imports
+import unittest
+
+# Local Imports
 from elections.us_states import postal_abbreviations
+from elections.query import query_data, temp_store_data
 
-def test_postal_abbreviations():
-    # confirm that there are 61 postal abbreviations in us_states.py
-    assert len(postal_abbreviations) == 61
 
-    # confirm that all states with current DW employees are in the list of states
-    dw_employee_states = ["CA", "CO", "DC", "IL", "KS", "KY", "MN", "NY", "PA", "RI", "VA", "WA"]
-    for state in dw_employee_states:
-        assert state in postal_abbreviations
+class TestPostalAbbreviations(unittest.TestCase):
+    """ Contains tests for postal_abbreviations list """
+
+    def test_postal_abbreviations(self):
+        # confirm that there are 61 postal abbreviations in us_states.py
+        self.assertEqual(len(postal_abbreviations), 61)
+
+        # confirm that all states with current DW employees are in the list of states
+        dw_employee_states = ["CA", "CO", "DC", "IL",
+                              "KS", "KY", "MN", "NY", "PA", "RI", "VA", "WA"]
+        for state in dw_employee_states:
+            self.assertIn(state, postal_abbreviations)
