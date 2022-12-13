bbc10a180570e5346da4ac4e0dd6c9a678f3c4dd
Gordon Stratton <gordon@democracy.works>
2022-12-13 09:54:03 -0800

    Update Clojure dependencies

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,10 +1,10 @@
 (defproject clj-upcoming-elections "0.1.0-SNAPSHOT"
   :description "An anonymous Democracy Works coding exercise"
   :min-lein-version "2.7.1"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [ring "1.9.4"]
-                 [ring/ring-defaults "0.3.3"]
-                 [compojure "1.6.2"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring "1.9.6"]
+                 [ring/ring-defaults "0.3.4"]
+                 [compojure "1.7.0"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler clj-upcoming-elections.core/handler})
